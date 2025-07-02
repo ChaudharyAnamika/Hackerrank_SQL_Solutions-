58adfa30da7b44676f97b652a2418f663833b8fd
ChaudharyAnamika <112795292+ChaudharyAnamika@users.noreply.github.com>
2025-07-02 19:15:17 +0530

    sql

---
+++ Hacker rank queries/18_Employee_Names.sql
@@ -0,0 +1,6 @@
+--QUES
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+--Ans
+SELECT name 
+FROM employee
+ORDER BY name ASC;
